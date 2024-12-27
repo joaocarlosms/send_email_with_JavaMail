76346f16b65d6da283508cc8a833913e188a3dce
joaocarlosms <jcmeseverino@gmail.com>
2024-12-27 16:51:33 -0300

    fat: add address of email and message to send

--- src/test/java/enviar_email/envia_email/AppTest.java
+++ src/test/java/enviar_email/envia_email/AppTest.java
@@ -2,10 +2,15 @@ package enviar_email.envia_email;
 
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Authenticator;
+import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,16 +18,16 @@ import org.junit.jupiter.api.Test;
  */
 public class AppTest {
 	String username = "jcjavaweb@gmail.com";
-	String password = "deathnote 04";
+	String password = "jlvg vikv almj gtzd";
 	
 	@Test
-	public void send_email() {
+	public void send_email() throws AddressException {
 		Properties properties = new Properties();
 		
 		try {
 			properties.put("mail.smtp.auth", "true");
 			properties.put("mail.smtp.starttls", "true");
-			properties.put("mail.smtp.host", "stmp.gmail.com");
+			properties.put("mail.smtp.host", "smtp.gmail.com");
 			properties.put("mail.smtp.port", "465");
 			properties.put("mail.smtp.socketFactory.port", "465");
 			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
@@ -34,9 +39,37 @@ public class AppTest {
 				}
 			});
 			
-			System.out.println(session.toString());
+			Address[] toUser = InternetAddress.parse("jcmedeiros04@gmail.com, jcjavaweb@gmail.com");
+			
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO, toUser);
+			message.setSubject("E-mail enviado com sucesso!");
+			message.setText("HELLO WORLD!!! BIBLICAAAAAL!");
+			
+			Transport.send(message);
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
