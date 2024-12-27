3fe59c577c7662ae389a2dd2eb119a60578f3da9
joaocarlosms <jcmeseverino@gmail.com>
2024-12-27 14:54:30 -0300

    feat: junit send_email configured for test

--- src/test/java/enviar_email/envia_email/AppTest.java
+++ src/test/java/enviar_email/envia_email/AppTest.java
@@ -1,19 +1,14 @@
 package enviar_email.envia_email;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+	
+	@Test
+	public void send_email() {
+		
+	}
 }
