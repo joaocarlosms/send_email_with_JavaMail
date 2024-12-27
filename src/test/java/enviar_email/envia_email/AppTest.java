7087ccc810df790c6fbe25512dc7caea3a8db06b
joaocarlosms <jcmeseverino@gmail.com>
2024-12-27 15:44:56 -0300

    feat: add properties of gmail

--- src/test/java/enviar_email/envia_email/AppTest.java
+++ src/test/java/enviar_email/envia_email/AppTest.java
@@ -1,5 +1,7 @@
 package enviar_email.envia_email;
 
+import java.util.Properties;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,6 +11,13 @@ public class AppTest {
 	
 	@Test
 	public void send_email() {
+		Properties properties = new Properties();
 		
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls", "true");
+		properties.put("mail.smtp.host", "stmp.gmail.com");
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.socketFactory.port", "465");
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 	}
 }
