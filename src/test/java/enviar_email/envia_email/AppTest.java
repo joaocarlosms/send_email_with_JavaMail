b3e786b13db919abd214282a57ffae32d12f0cff
joaocarlosms <jcmeseverino@gmail.com>
2024-12-27 16:01:36 -0300

    feat: add session for authentication

--- src/test/java/enviar_email/envia_email/AppTest.java
+++ src/test/java/enviar_email/envia_email/AppTest.java
@@ -2,22 +2,41 @@ package enviar_email.envia_email;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+	String username = "jcjavaweb@gmail.com";
+	String password = "deathnote 04";
 	
 	@Test
 	public void send_email() {
 		Properties properties = new Properties();
 		
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.starttls", "true");
-		properties.put("mail.smtp.host", "stmp.gmail.com");
-		properties.put("mail.smtp.port", "465");
-		properties.put("mail.smtp.socketFactory.port", "465");
-		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		try {
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.starttls", "true");
+			properties.put("mail.smtp.host", "stmp.gmail.com");
+			properties.put("mail.smtp.port", "465");
+			properties.put("mail.smtp.socketFactory.port", "465");
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+			
+			System.out.println(session.toString());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
