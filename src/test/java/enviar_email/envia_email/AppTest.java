7d50f1b35f80f9e5adb530239e5c52de4458b503
joaocarlosms <jcmeseverino@gmail.com>
2024-12-28 15:23:33 -0300

    feat: instance object in JUnit test

--- src/test/java/enviar_email/envia_email/AppTest.java
+++ src/test/java/enviar_email/envia_email/AppTest.java
@@ -1,17 +1,7 @@
 package enviar_email.envia_email;
 
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import org.junit.jupiter.api.Test;
+
 
 /**
  * Unit test for simple App.
@@ -21,38 +11,14 @@ public class AppTest {
 	String password = "jlvg vikv almj gtzd";
 	
 	@Test
-	public void send_email() throws AddressException {
-		Properties properties = new Properties();
+	public void send_email() throws Exception {
+		ObjectSendEmail send_email = new ObjectSendEmail(
+				"jcmedeiros04@gmail.com, jcjavaweb@gmail.com",
+				"jc-dev",
+				"E-mail received with success using Java!",
+				"You have a new message in your email!");
 		
-		try {
-			properties.put("mail.smtp.ssl.trust", "*");
-			properties.put("mail.smtp.auth", "true");
-			properties.put("mail.smtp.starttls", "true");
-			properties.put("mail.smtp.host", "smtp.gmail.com");
-			properties.put("mail.smtp.port", "465");
-			properties.put("mail.smtp.socketFactory.port", "465");
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			
-			Session session = Session.getInstance(properties, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(username, password);
-				}
-			});
-			
-			Address[] toUser = InternetAddress.parse("jcmedeiros04@gmail.com, jcjavaweb@gmail.com");
-			
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(username, "jc-dev"));
-			message.setRecipients(Message.RecipientType.TO, toUser);
-			message.setSubject("E-mail enviado com sucesso!");
-			message.setText("HELLO WORLD!!! BIBLICAAAAAL!");
-			
-			Transport.send(message);
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		send_email.sendEmail();
 	}
 }
 
